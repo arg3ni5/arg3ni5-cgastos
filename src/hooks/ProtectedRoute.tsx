4a52d9f4e85749dd840128b1d0ecd356f0c977c9
Fabian Chinchilla Mayorga <fb.argenis@gmail.com>
2025-03-29 10:09:56 -0600

    refactor(hooks): update ProtectedRoute component with TypeScript interface and useUserAuth hook
    Updated the ProtectedRoute component to include a TypeScript interface for better type safety and clarity. Additionally, replaced the UserAuth hook with useUserAuth in the routes configuration for consistency and improved readability.

--- src/hooks/ProtectedRoute.tsx
+++ src/hooks/ProtectedRoute.tsx
@@ -1,5 +1,13 @@
 import { Navigate, Outlet } from "react-router-dom";
-export const ProtectedRoute = ({ user, redirectTo, children }) => {
+import { ReactNode } from "react";
+
+interface ProtectedRouteProps {
+  user: unknown; // Podés usar un tipo más preciso si tenés uno (ej. `UserMetadata`)
+  redirectTo: string;
+  children?: ReactNode;
+}
+
+export const ProtectedRoute = ({ user, redirectTo, children }: ProtectedRouteProps) => {
   if (user == null) return <Navigate replace to={redirectTo} />;
-  return children ? children : <Outlet />;
+  return children ? <>{children}</> : <Outlet />;
 };
