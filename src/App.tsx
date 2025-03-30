6247b1e88fc976e877cf642f135258560b5fe753
Fabian Chinchilla Mayorga <fabian.chinchillam@euromobilia.com>
2025-03-30 14:06:53 -0600

    refactor(Carousel): improve navigation and code structure
    - Replace default Swiper navigation with custom buttons using React Icons
    - Optimize Swiper slide rendering with a map function for better maintainability
    - Lazy load Swiper CSS modules to improve performance
    - Clean up and organize styled components for better readability

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App(): JSX.Element {
                 </div>
 
                 <Containerbody>
-                  <MyRoutes />
+                  <MyRoutes isLoading={isLoading}/>
                 </Containerbody>
               </Container>
             ) : (
